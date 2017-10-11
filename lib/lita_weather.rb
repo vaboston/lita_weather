3746521c35b155fbbf1efff14aa4aa0729f6de40
Valentin Bouin <valentin.bouin@fr.clara.net>
2017-10-11 14:08:18 +0200

    add old files

--- lib/lita_weather.rb
+++ lib/lita_weather.rb
@@ -1,5 +1,5 @@
-require "lita_weather/version"
+require "lita"
 
-module LitaWeather
-  # Your code goes here...
-end
+Lita.load_locales Dir[File.expand_path(
+	  File.join("..", "..", "locales", "*.yml"), __FILE__
+)]
--- lib/lita_weather/version.rb
+++
@@ -1,3 +0,0 @@
-module LitaWeather
-  VERSION = "0.1.0"
-end
